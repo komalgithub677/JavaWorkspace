7d0b1a47c6147ee0bdb0467c8198ba148eb59ee6
Komal Narawade <knarawade4@gmail.com>
2025-10-30 18:43:08 +0530

    Updated Java workspace code

---
+++ DataTypes/src/Day1Variable.java
@@ -0,0 +1,21 @@
+
+public class Day1Variable {
+	//global variables
+       public static void main(String[] args) {
+    	   //local variables
+    	String stud_Name = "Komal";
+    	int myAge = 24;
+    	int rollNo = 123;
+    	String email ="knarawade4@gmail.com";
+    	float marks = 78.56f;
+    	boolean isPassed = true;
+    	
+    	
+		System.out.println(stud_Name);
+		System.out.println(myAge);
+		System.out.println(rollNo);
+		System.out.println(email);
+		System.out.println(marks);
+		System.out.println(isPassed);
+	}
+}
