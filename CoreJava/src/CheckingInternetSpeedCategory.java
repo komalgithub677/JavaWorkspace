e7d8c3955456408cb0135486ca236c260ec00f20
Komal Narawade <knarawade4@gmail.com>
2025-11-15 18:42:42 +0530

    Checking Internetspeed mbps level

---
+++ CoreJava/src/CheckingInternetSpeedCategory.java
@@ -0,0 +1,33 @@
+package ControlFlow;
+
+public class CheckingInternetSpeedCategory {
+       public static void main(String[] args) {
+		int speed = 10;
+		if(speed>=0) {
+			if(speed<=5) {
+				System.out.println("Very slow internet speed");
+			}
+			else if(speed<=25) {
+				System.out.println("Average internet speed basic browsing");
+			}
+			else if( speed<=50) {
+				System.out.println("Good internet speed");
+			}
+			else if(speed<=100) {
+				System.out.println("Fast internet speed");
+			}
+			else if( speed<=500) {
+				System.out.println("Very-fast internet speed");
+			}
+			else if( speed<=1000) {
+				System.out.println("Ultra fast Internet");
+			}
+			else {
+				System.out.println("Extreme speed");
+			}
+		}
+		else {
+			System.out.println("Invalid speed");
+		}
+	}
+}
