cdbc56fcb341decaa0a8b7d6f18f2536a233455f
Komal Narawade <knarawade4@gmail.com>
2025-11-18 15:31:46 +0530

    Hostel Room Type

---
+++ CoreJava/src/SwitchCase/HotelRoomType.java
@@ -0,0 +1,30 @@
+package SwitchCase;
+
+public class HotelRoomType {
+             public static void main(String[] args) {
+            	// 27. Hotel Room Type
+
+            	 //1: Standard
+            	 //2: Deluxe
+            	 //3: Suite
+            	 
+            	 int roomType =1;
+            	 
+            	 switch(roomType) {
+            	 case 1:
+            		 System.out.println("Room type would be the standard");
+            	 break;
+            	 
+            	 case 2:
+            		 System.out.println("Room type would be the deluxe");
+            	 break;
+            	 
+            	 case 3:
+            		 System.out.println("Room type would be the suite");
+            	 break;
+            	 
+            	 default:
+            		 System.out.println("Selected room is not available");
+            	 }
+			}
+}
