0544332008e5dc960935bc6e5c191f7ff6794b4d
Komal Narawade <knarawade4@gmail.com>
2025-11-20 20:44:51 +0530

    Number even or odd

---
+++ CoreJava/src/loops/NumbersEvenOrOdd.java
@@ -0,0 +1,18 @@
+package loops;
+
+public class NumbersEvenOrOdd {
+       public static void main(String[] args) {
+    	   int evenCount = 0;
+    	   int oddCount = 0;
+		for(int i=1; i<=10; i++) {
+			if(i%2==0) {
+				evenCount++;
+			}
+			else {
+				oddCount++;
+			}
+		}
+		System.out.println("Even Count = " + evenCount);
+		System.out.println("Odd Count = " + oddCount);
+	}
+}
