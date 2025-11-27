c677bbeaf80881a5c12e4357a3639d4afa589aa9
Komal Narawade <knarawade4@gmail.com>
2025-11-27 13:19:09 +0530

    Day 29

---
+++ CoreJava/src/OOPS/Car.java
@@ -0,0 +1,21 @@
+package OOPS;
+
+public class Car {
+	         //Global variables
+	         String brand;
+	         String color;
+	         int price;
+	         
+             public static void main(String[] args) {
+				
+            	 Car c = new Car();
+            	 //local variables
+            	 String brand = c.brand = "Toyota";
+            	 String color = c.color ="Black";
+            	 int price = c.price =3000000;
+            	 
+            	 System.out.println("Brand of car :" + brand);
+            	 System.out.println("Color of car : " +color );
+            	 System.out.println("Price of car : " + price);  	 
+            	 }
+}
