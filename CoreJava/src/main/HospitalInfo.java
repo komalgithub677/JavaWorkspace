5d8c47a309b76d7eb53d28b2080afd4869ac5a08
Komal Narawade <knarawade4@gmail.com>
2025-11-28 13:22:11 +0530

    Day 30

---
+++ CoreJava/src/main/HospitalInfo.java
@@ -0,0 +1,12 @@
+package main;
+
+import oops.Hospital;
+public class HospitalInfo {
+       public static void main(String[] args) {
+		Hospital h1 = new Hospital();
+		
+		h1.name ="City Care Hospital";
+		h1.
+		
+	}
+}
--- CoreJava/src/oops/Hospital.java
+++ CoreJava/src/oops/Hospital.java
@@ -1,5 +1,15 @@
 package oops;
 
 public class Hospital {
-
+	    public String name;
+	    public String type;
+	    public String speciality;
+	    public int numberOfDoctors;
+	    public int numberOfbeds;
+	    public String specialisation;
+	    public String contactNumber;
+	    
+       public static void main(String[] args) {
+		
+	}
 }
