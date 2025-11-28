d3a04268295be579dfbb2bed5ebba944a2a6fb20
Komal Narawade <knarawade4@gmail.com>
2025-11-28 18:15:38 +0530

    Day 30

--- CoreJava/src/Method/Employee.java
+++ CoreJava/src/Method/Employee.java
@@ -4,15 +4,26 @@ public class Employee {
             
 	//returnType methodName(){}
 	 //Non-parameterised without return type
-	void m1() {
+	void m2() {
 		System.out.println("Hello world");
 	}
+	//Parameterised method with return type
+	void m1(String fName , String Lname) {
+		System.out.println("Hello world");
+		System.out.println(fName + " " + Lname);
+	}
+	
 	
 	void myAddition(int a , int b) {
 //		int a =23;
 //		int b =2;
 		
 		System.out.println("Sum is = " + (a+b));
+	}
+	
+	void mySquare() {
+		int num = 12;
+		System.out.println("Sauare of  "+ num + "= " + (num*num));
 	}
 	
 	
--- CoreJava/src/Method/EmployeeInfo.java
+++ CoreJava/src/Method/EmployeeInfo.java
@@ -5,10 +5,11 @@ public class EmployeeInfo {
        
        public static void main(String[] args) {
 		Employee e = new Employee();
-		e.m1();
-		e.m1();
-		e.m1();
+		e.m1("Komal" , "Narawade");
+		e.m1("Monika" , "Jadhav");
+		e.m1("Divya" , "Patil");
 		e.myAddition(10 ,30);
+		e.mySquare();
 		
 	}
 }
