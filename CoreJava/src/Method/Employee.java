86b8feda37934151dda614ede03426f56dfae40a
Komal Narawade <knarawade4@gmail.com>
2025-11-28 17:39:51 +0530

    Day 30

---
+++ CoreJava/src/Method/Employee.java
@@ -0,0 +1,11 @@
+package Method;
+
+public class Employee {
+            
+	//returnType methodName(){}
+	 
+	void m1() {
+		System.out.println("Hello world");
+	}
+	
+}
---
+++ CoreJava/src/Method/EmployeeInfo.java
@@ -0,0 +1,10 @@
+package Method;
+
+
+public class EmployeeInfo {
+       
+       public static void main(String[] args) {
+		Employee e = new Employee();
+		e.m1();
+	}
+}
