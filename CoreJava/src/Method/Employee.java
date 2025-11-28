bcd74532831ca15ec6026e8a1f727e9a15e3a78c
Komal Narawade <knarawade4@gmail.com>
2025-11-28 18:01:57 +0530

    Day 30

--- CoreJava/src/Method/Employee.java
+++ CoreJava/src/Method/Employee.java
@@ -3,9 +3,17 @@ package Method;
 public class Employee {
             
 	//returnType methodName(){}
-	 
+	 //Non-parameterised without return type
 	void m1() {
 		System.out.println("Hello world");
 	}
+	
+	void myAddition(int a , int b) {
+//		int a =23;
+//		int b =2;
+		
+		System.out.println("Sum is = " + (a+b));
+	}
+	
 	
 }
--- CoreJava/src/Method/EmployeeInfo.java
+++ CoreJava/src/Method/EmployeeInfo.java
@@ -6,5 +6,9 @@ public class EmployeeInfo {
        public static void main(String[] args) {
 		Employee e = new Employee();
 		e.m1();
+		e.m1();
+		e.m1();
+		e.myAddition(10 ,30);
+		
 	}
 }
