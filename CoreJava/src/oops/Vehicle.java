9b7aa752ac7152465ca179452677a696fed0c95a
Komal Narawade <knarawade4@gmail.com>
2025-11-28 12:00:11 +0530

    Day 30

--- CoreJava/src/oops/Vehicle.java
+++ CoreJava/src/oops/Vehicle.java
@@ -1,6 +1,25 @@
 package oops;
 
 public class Vehicle {
-              String name;
+              public String name;
+              public String ownerName;
+              public String color;
+              public double price;
+              public int speed;
+              public int wheels;
+              public int registration_number;
+              public String model;
+              public int year;
+              public int airbags;
+              public double mileage;
+              
+              public static void main(String[] args) {
+				
+            	  
+			}
+              
+              
+              
+              
               
 }
