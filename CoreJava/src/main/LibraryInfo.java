07464aecdbb6fd47eb76f81962bd8198773614a0
Komal Narawade <knarawade4@gmail.com>
2025-11-28 12:40:18 +0530

    Day 30

---
+++ CoreJava/src/main/LibraryInfo.java
@@ -0,0 +1,99 @@
+package main;
+
+import oops.Library;
+
+public class LibraryInfo {
+     public static void main(String[] args) {
+    	//Creation of first objects from teh library class
+		Library l1 = new Library();
+		
+		String name1 =l1.libraryName ="Knowledge Hub Library";
+		String email1=l1.Libraymail="knowledgehub@gmail.com";
+		long contact1 =l1.contactNo=9867957689l;
+		String openingTime1 =l1.openingTime="9:00 Am";
+		String closingTime1 =l1.closingTime="18:00 pm";
+		int noOfBooks = l1.noOfBooks=50000;
+		
+		System.err.println("Library 1 Info");
+		System.out.println("Name :" + name1);
+		System.out.println("email :" + email1);
+		System.out.println("contact : " + contact1);
+        System.out.println("Openinig Time : " + openingTime1);
+        System.out.println("Closing Time : " + closingTime1);
+        System.out.println("No of Books : " + noOfBooks);
+        
+        //creation of the second object from the library class
+        Library l2 = new Library();
+		
+		String name2 =l2.libraryName ="City Corner Public Library";
+		String email2=l2.Libraymail="citycornerpublic@gmail.com";
+		long contact2 =l2.contactNo=9867957684l;
+		String openingTime2 =l2.openingTime="9:00 Am";
+		String closingTime2 =l2.closingTime="18:00 pm";
+		int noOfBooks2 = l2.noOfBooks=40000;
+		
+		System.err.println("Library 2 Info");
+		System.out.println("Name :" + name2);
+		System.out.println("email :" + email2);
+		System.out.println("contact : " + contact2);
+        System.out.println("Openinig Time : " + openingTime2);
+        System.out.println("Closing Time : " + closingTime2);
+        System.out.println("No of Books : " + noOfBooks2);
+        
+        
+       Library l3 = new Library();
+		
+		String name3 =l3.libraryName ="Modern Study Library";
+		String email3=l3.Libraymail="modernstudy@gmail.com";
+		long contact3 =l3.contactNo=9867957685l;
+		String openingTime3 =l3.openingTime="9:00 Am";
+		String closingTime3 =l3.closingTime="18:00 pm";
+		int noOfBooks3 = l3.noOfBooks=30000;
+		
+		System.err.println("Library 3 Info");
+		System.out.println("Name :" + name3);
+		System.out.println("email :" + email3);
+		System.out.println("contact : " + contact3);
+        System.out.println("Openinig Time : " + openingTime3);
+        System.out.println("Closing Time : " + closingTime3);
+        System.out.println("No of Books : " + noOfBooks3);
+        
+       Library l4 = new Library();
+		
+		String name4 =l4.libraryName ="Readers Paradise Library";
+		String email4=l4.Libraymail="redarparadiselibrary@gmail.com";
+		long contact4 =l4.contactNo=9867957665l;
+		String openingTime4 =l4.openingTime="9:00 Am";
+		String closingTime4 =l4.closingTime="18:00 pm";
+		int noOfBooks4 = l4.noOfBooks=20000;
+		
+		System.err.println("Library 4 Info");
+		System.out.println("Name :" + name4);
+		System.out.println("email :" + email4);
+		System.out.println("contact : " + contact4);
+        System.out.println("Openinig Time : " + openingTime4);
+        System.out.println("Closing Time : " + closingTime4);
+        System.out.println("No of Books : " + noOfBooks4);
+        
+       Library l5 = new Library();
+		
+		String name5 =l5.libraryName ="City Corner Public Library";
+		String email5=l5.Libraymail="centralcity@gmail.com";
+		long contact5 =l5.contactNo=9867957685l;
+		String openingTime5 =l5.openingTime="9:00 Am";
+		String closingTime5 =l5.closingTime="18:00 pm";
+		int noOfBooks5 = l5.noOfBooks=22000;
+		
+		System.err.println("Library 5 Info");
+		System.out.println("Name :" + name5);
+		System.out.println("email :" + email5);
+		System.out.println("contact : " + contact5);
+        System.out.println("Openinig Time : " + openingTime5);
+        System.out.println("Closing Time : " + closingTime5);
+        System.out.println("No of Books : " + noOfBooks5);
+        
+		
+		
+		
+	}
+}
