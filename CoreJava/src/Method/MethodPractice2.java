a6cde7752a741d505e1929e0f4d918afa162fbee
Komal Narawade <knarawade4@gmail.com>
2025-11-30 10:30:46 +0530

    Day 31

---
+++ CoreJava/src/Method/MethodPractice2.java
@@ -0,0 +1,43 @@
+package Method;
+
+public class MethodPractice2 {
+//	Methods without return type & with arguments
+//
+//			Write a void method that takes a number and prints whether it is even or odd.
+            public void PrintNumberEvenOdd(int n) {
+            	if(n%2==0) {
+            		System.out.println("Number is even ");
+            	}
+            	else {
+            		System.out.println("Number is odd");
+            	}
+            	
+            }
+	
+//			Create a method that accepts two numbers and prints the larger one.
+            public void largerNumber(int a , int b) {
+                        if(a>b) {
+                        	System.out.println("a is largest");
+                        }
+                        else{
+                        	System.out.println("b is largest");
+                        }
+            }
+//
+//			Write a method that receives a string and prints its reverse.
+            
+//
+//			Create a method with three integers and print their average.
+//
+//			Write a method that takes a number and prints its multiplication table.
+//
+//			Create a method that accepts a number and checks if it is prime.
+//
+//			Write a method that takes a string and count vowels inside it.
+//
+//			Create a method that accepts temperature in Celsius and prints Fahrenheit.
+//
+//			Write a method that takes an amount and discount % and prints the final price.
+//
+//			Create a method that receives an array and prints the maximum element.
+}
---
+++ CoreJava/src/Method/MethodPractice2Info.java
@@ -0,0 +1,11 @@
+package Method;
+
+public class MethodPractice2Info {
+             
+	public static void main(String [] args) {
+		          MethodPractice2 m1 = new MethodPractice2();
+		          
+		          m1.PrintNumberEvenOdd(34);
+		          
+	}
+}
