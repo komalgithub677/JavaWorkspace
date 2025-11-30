b54614a21494507032f5d306059189057794d809
Komal Narawade <knarawade4@gmail.com>
2025-11-30 22:07:50 +0530

    Day 31

--- CoreJava/src/Method/MethodPractice4.java
+++ CoreJava/src/Method/MethodPractice4.java
@@ -5,10 +5,49 @@ public class MethodPractice4 {
 //	🔥 SECTION B — If, Else, Else-If Questions
 //
 //	Write a program using if-else to check whether a person is eligible to vote.
+	public void eligible() {
+		int age =18;
+		if(age>=18) {
+			System.out.println("Person is eligible to vote");
+		}
+		else {
+			System.out.println("Person is not eligible to vote");
+		}
+	}
 //
 //	Accept marks and print grade: A,B,C,D,Fail using else-if ladder.
+	 public void PrintGrade() {
+		 int marks =45;
+		 if(marks<=100 && marks>=90) {
+			 System.out.println("Grade A");
+		 }
+		 else if(marks<90 && marks>=75) {
+			 System.out.println("Grade B");
+		 }
+		 else if(marks<75 && marks>=60) {
+			 System.out.println("Grade C");
+		 }
+		 else if(marks>=35 && marks<60) {
+			 System.out.println("Grade D");
+		 }
+		 else {
+			 System.out.println("Fail");
+		 }
+	 }
 //
 //	Check whether the number is positive, negative or zero.
+	 public void NumberPositiveNegativeZero() {
+		 int num =10;
+		 if(num==0) {
+			 System.out.println("Number is zero");
+		 }
+		 else if(num>0) {
+			 System.out.println("Number is positive");
+		 }
+		 else {
+			 System.out.println("Number is negative");
+		 }
+	 }
 //
 //	Check whether a character is vowel or consonant.
 //
@@ -23,4 +62,4 @@ public class MethodPractice4 {
 //	Input salary and calculate bonus based on conditions.
 //
 //	Check if entered login credentials are valid (username + password).
-//}
+}
--- CoreJava/src/Method/MethodPractice4Info.java
+++ CoreJava/src/Method/MethodPractice4Info.java
@@ -2,6 +2,8 @@ package Method;
 
 public class MethodPractice4Info {
              public static void main(String[] args) {
-				
+				MethodPractice4 m4 = new MethodPractice4();
+				m4.eligible();
+				m4.PrintGrade();
 			}
 }
