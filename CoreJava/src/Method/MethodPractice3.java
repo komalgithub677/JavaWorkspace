ffe73c3be7bc80c828838b6f2487db5c05007458
Komal Narawade <knarawade4@gmail.com>
2025-11-30 12:25:01 +0530

    Day 31

---
+++ CoreJava/src/Method/MethodPractice3.java
@@ -0,0 +1,38 @@
+package Method;
+
+public class MethodPractice3 {
+//	💡 Methods without return type & without arguments
+//
+//			Write a void method that prints the first 10 natural numbers.
+//
+	        public void NaturalNumber() {
+	        	for(int i=1;  i<=10; i++) {
+	        		System.out.println(i);
+	        	}
+	        }
+//			Create a method that prints "Welcome to Java Programming".
+	       public void Welcome () {
+	    	   System.out.println("Welcome to Java Programming");
+	       }
+//
+//			Write a method that prints the sum of numbers from 1 to 100.
+//
+//			Create a method that prints all even numbers between 1–50.
+//
+//			Write a method that prints factorial of 5.
+//
+//			Create a method that prints Fibonacci series up to 10 terms.
+//
+//			Write a method that prints table of 9 without taking input.
+//
+//			Create a method that prints your name 5 times.
+//
+//			Write a method that prints numbers divisible by 7 from 1–100.
+//
+//			Create a method that prints pattern:
+//
+//			*
+//			**
+//			***
+//			****
+}
---
+++ CoreJava/src/Method/MethodPractice3Info.java
@@ -0,0 +1,11 @@
+package Method;
+
+public class MethodPractice3Info {
+	
+             public static void main(String[] args) {
+            	 MethodPractice3 m3 = new MethodPractice3();
+                 m3.NaturalNumber();
+                 m3.Welcome();
+			}
+}
+
