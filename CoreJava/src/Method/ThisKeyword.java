26effd78b7326bc42dd0b0344d9bb360cad197ba
Komal Narawade <knarawade4@gmail.com>
2025-12-02 18:19:17 +0530

    Day 32

--- CoreJava/src/Method/ThisKeyword.java
+++ CoreJava/src/Method/ThisKeyword.java
@@ -1,5 +1,25 @@
 package Method;
 
 public class ThisKeyword {
-        
+             int BranchId;
+             String BankName;
+             String ifscCode;
+             long accountNo;
+             String city;
+             
+             public void bankDetails(int BranchId , String bankName ,String ifscCode,long accNo , String city) {
+            	this.BranchId = BranchId;
+            	this.BankName = BankName;
+            	this.ifscCode =ifscCode;
+            	this.accountNo=accountNo;
+            	this.city = city;
+            	
+            	System.out.println("Bank Deatils :");
+            	System.out.println("Branch id :" + BranchId);
+            	System.out.println("IFSC Code :" + ifscCode);
+            	System.out.println("Account Number :" + accountNo);
+            	System.out.println("Bank City :" + city);
+            	 return;
+             }
+             
 }
---
+++ CoreJava/src/Method/ThisKeywordInfo.java
@@ -0,0 +1,11 @@
+package Method;
+
+public class ThisKeywordInfo {
+             public static void main(String[] args) {
+				ThisKeyword t =new ThisKeyword();
+				t.bankDetails(121, "SBI", "SBIN0004567", 23546766767676l, "Pune");
+				
+				
+				
+			}
+}
