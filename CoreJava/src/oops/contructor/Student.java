aaf3ea9c433780b28555d32f81971248816a998c
Komal Narawade <knarawade4@gmail.com>
2025-12-04 18:39:00 +0530

    Day 33

--- CoreJava/src/oops/contructor/Student.java
+++ CoreJava/src/oops/contructor/Student.java
@@ -9,6 +9,32 @@ public class Student {
              String dob;
              String address;
              
+             Student(int age,
+             String name,
+             String city,
+             int rollNo,
+             String gender,
+             String dob,
+             String address){
+            	 this.name=name;
+            	 this.age=age;
+            	 this.city=city;
+            	 this.dob=dob;
+            	 this.gender=gender;
+            	 this.rollNo=rollNo;
+                 this.address=address;
+                 student();
+             }
+             
+             void student() {
+            	 System.out.println("Student name" +name);
+            	 System.out.println("Student age"+age);
+            	 System.out.println("Student city" + city);
+            	 System.out.println("Student Date Of Birth" + dob);
+            	 System.out.println("Student gender :" + gender);
+            	 System.out.println("Student rollNo :" + rollNo);
+            	 System.out.println("Student address :" + address);
+             }
              
 	
 }
--- CoreJava/src/oops/contructor/StudentManagement.java
+++ CoreJava/src/oops/contructor/StudentManagement.java
@@ -1,5 +1,7 @@
 package oops.contructor;
 
 public class StudentManagement {
-
+             public static void main(String[] args) {
+				    Student s = new Student(12, "Komal", "Pune", 0, "", null, null);	           
+			}
 }
