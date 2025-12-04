38d2dcda68b6ab66094261e6dcecaa20617ad7fb
Komal Narawade <knarawade4@gmail.com>
2025-12-04 18:29:25 +0530

    Day 33

---
+++ CoreJava/src/oops/contructor/EmpInfo.java
@@ -0,0 +1,10 @@
+package oops.contructor;
+
+public class EmpInfo {
+            public static void main(String[] args) {
+				Employee e = new Employee(12);
+				Employee e1 = new Employee(12,"Komal","30lpa" ,"Pune");
+				e.view();
+				
+			}
+}
---
+++ CoreJava/src/oops/contructor/Student.java
@@ -0,0 +1,14 @@
+package oops.contructor;
+
+public class Student {
+             int age;
+             String name;
+             String city;
+             int rollNo;
+             String gender;
+             String dob;
+             String address;
+             
+             
+	
+}
