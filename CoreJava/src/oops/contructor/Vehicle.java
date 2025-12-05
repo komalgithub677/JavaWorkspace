808d224c6a1ae6632e2a9f8daa639070546a9b6b
Komal Narawade <knarawade4@gmail.com>
2025-12-05 21:08:16 +0530

    Day 34

---
+++ CoreJava/src/oops/contructor/Vehicle.java
@@ -0,0 +1,26 @@
+package oops.contructor;
+
+public class Vehicle {
+//	4. Create a class Car with private fields brand and price.
+//	Provide setters and getters and print details.**
+	
+	private String brand;
+	private double price;
+	
+	public String getBrand() {
+		return brand;
+	}
+	
+	public void setBrand(String brand) {
+		this.brand =brand;
+	}
+	
+	public Double getPrice() {
+		return price;
+	}
+	
+	public void setPrice(double price) {
+		this.price =price;
+	}
+
+}
