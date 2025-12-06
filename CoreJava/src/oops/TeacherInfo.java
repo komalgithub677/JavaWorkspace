5668886a094455c99eef6aa191fb4367bf80374b
Komal Narawade <knarawade4@gmail.com>
2025-12-06 23:10:59 +0530

    Day 35

---
+++ CoreJava/src/oops/TeacherInfo.java
@@ -0,0 +1,17 @@
+package oops;
+
+public class TeacherInfo {
+             public static void main(String[] args) {
+				Teacher t = new Teacher();
+				String name = t.name ="Kusumlata mam";
+				String subject =t.subject ="CNS";
+				int experience = t.experience =4;
+				double salary = t.salary =40000.00;
+				
+				System.out.println("Teacher Deatails : ");
+				System.out.println("Name:" + name);
+				System.out.println("Subject : " + subject);
+				System.out.println("Experice : " + experience);
+				System.out.println("Salary : " + salary);
+			}
+}
