d890c02a5605b755d480245a1aedd5d35df61cb8
Komal Narawade <knarawade4@gmail.com>
2025-12-07 22:13:42 +0530

    Day 36

---
+++ CoreJava/src/encapsulation/PersonAgeInfo.java
@@ -0,0 +1,9 @@
+package encapsulation;
+
+public class PersonAgeInfo {
+             public static void main(String[] args) {
+				PersonAge p = new PersonAge();
+				p.setAge(12);
+				System.out.println("Age :" + p.getAge());
+			}
+}
