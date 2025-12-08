835f574c64728ccf8151e0e5df7c6a64fb0fec37
Komal Narawade <knarawade4@gmail.com>
2025-12-08 22:47:42 +0530

    Day 36

---
+++ CoreJava/src/oops/inheritance/Application.java
@@ -0,0 +1,13 @@
+package oops.inheritance;
+
+public class Application {
+             public void register() {
+            	 System.out.println("Registration succefull");
+             }
+             public void login() {
+            	 System.out.println("Login successfull");
+             }
+             public void pay() {
+            	 System.out.println("Payment successfully Transfered");
+             }
+}
