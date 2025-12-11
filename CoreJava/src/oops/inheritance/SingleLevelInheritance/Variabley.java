b058d19515bdbd02bf1931e5e9c4a2ae9ac7b86c
Komal Narawade <knarawade4@gmail.com>
2025-12-11 16:35:59 +0530

    Day 37

---
+++ CoreJava/src/oops/inheritance/SingleLevelInheritance/Variabley.java
@@ -0,0 +1,15 @@
+package oops.inheritance.SingleLevelInheritance;
+
+public class Variabley extends Variablex {
+              public int y;
+              
+              public void variableY(int y) {
+            	  this.y = y;
+              }
+              public static void main(String[] args) {
+				Variabley y = new Variabley();
+				System.out.println(y.variableX(20));
+				System.out.println();
+				
+			}
+}
