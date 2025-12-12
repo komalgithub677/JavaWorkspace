420faa176a92b026a7e5927e3a2753d17c9183b0
Komal Narawade <knarawade4@gmail.com>
2025-12-12 17:59:33 +0530

    Day 38

---
+++ CoreJava/src/scanner/TableOfNumber.java
@@ -0,0 +1,15 @@
+package scanner;
+
+import java.util.Scanner;
+
+public class TableOfNumber {
+             public static void main(String[] args) {
+				Scanner sc =new Scanner(System.in);
+				System.out.println("Enter your number");
+				
+				int num = sc.nextInt();
+				for(int i=1; i<=10; i++) {
+					System.out.println(i*num);
+				}
+			}
+}
