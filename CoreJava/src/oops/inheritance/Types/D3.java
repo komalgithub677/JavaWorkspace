bc7671885998e86111c829bd4458865720199038
Komal Narawade <knarawade4@gmail.com>
2025-12-12 23:35:12 +0530

    Day 38

---
+++ CoreJava/src/oops/inheritance/Types/D3.java
@@ -0,0 +1,11 @@
+package oops.inheritance.Types;
+
+public class D3 {
+             public void d3() {
+            	 System.out.println("D3 Method called");
+             }
+             public static void main(String[] args) {
+				D3 d3 = new D3();
+				d3.d3();
+			}
+}
