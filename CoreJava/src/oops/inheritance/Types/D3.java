d753396fd0913a149fa0e8a1c924408e76f61aa8
Komal Narawade <knarawade4@gmail.com>
2025-12-12 23:42:01 +0530

    Day 38

--- CoreJava/src/oops/inheritance/Types/D3.java
+++ CoreJava/src/oops/inheritance/Types/D3.java
@@ -1,11 +1,12 @@
 package oops.inheritance.Types;
 
-public class D3 {
+public class D3 extends C3 {
              public void d3() {
             	 System.out.println("D3 Method called");
              }
              public static void main(String[] args) {
 				D3 d3 = new D3();
+				d3.a3();
 				d3.d3();
 			}
 }
