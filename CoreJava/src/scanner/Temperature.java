bfbca979676691226f76e714f802b68f0c31d449
Komal Narawade <knarawade4@gmail.com>
2025-12-13 22:39:36 +0530

    Day 39

--- CoreJava/src/scanner/Temperature.java
+++ CoreJava/src/scanner/Temperature.java
@@ -1,5 +1,20 @@
 package scanner;
 
-public class Temperature {
+import java.util.Scanner;
 
+public class Temperature {
+             public void temperature(double celcius) {
+            	         double fahrenheit = celcius *(9/5) + 32;
+            	         System.out.println("Temperature in fahrenheit is :" + fahrenheit);
+             }
+             public static void main(String[] args) {
+				Scanner sc = new Scanner(System.in);
+				
+				System.out.println("Enter the temperature in Celcius : ");
+				
+				double fahreinheit = sc.nextDouble();
+				System.out.println("Fahrenheit Temperature : " + fahreinheit);
+	
+			}
+             
 }
