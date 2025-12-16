9986211bbca907c04b587e3a447157b10de580a2
Komal Narawade <knarawade4@gmail.com>
2025-12-16 17:44:49 +0530

    Day 39

---
+++ CoreJava/src/oops/Polymorphism/overload/Calculator.java
@@ -0,0 +1,20 @@
+package oops.Polymorphism.overload;
+
+public class Calculator {
+             void myAddition(int a ,int b) {
+            	 System.out.println(a+b);
+             }
+             void myAddition(float a ,float b) {
+            	 System.out.println(a+b);
+             }
+             void myAddition(double a ,double b) {
+            	 System.out.println(a+b);
+             }
+             
+             public static void main(String[] args) {
+				Calculator c = new Calculator();
+				c.myAddition(12, 23);
+				c.myAddition(23.00, 45.00);
+				c.myAddition(45.00, 46.00);
+			}
+}
--- CoreJava/src/oops/Polymorphism/overload/MyInfo.java
+++ CoreJava/src/oops/Polymorphism/overload/MyInfo.java
@@ -4,11 +4,9 @@ public class MyInfo {
        public void myName(String fName) {
     	   System.out.println(fName);
        }
-       
        public void myName(String fName , String lName) {
     	   System.out.println(fName + " " + lName);
        }
-       
        public static void main(String[] args) {
 		MyInfo m = new MyInfo();
 		m.myName(null);
