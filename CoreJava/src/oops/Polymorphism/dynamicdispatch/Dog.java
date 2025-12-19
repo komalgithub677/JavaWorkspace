46a91515a4942647928369565f4c15c55a3abeeb
Komal Narawade <knarawade4@gmail.com>
2025-12-19 13:59:53 +0530

    Day 42

---
+++ CoreJava/src/oops/Polymorphism/dynamicdispatch/Dog.java
@@ -0,0 +1,7 @@
+package oops.Polymorphism.dynamicdispatch;
+
+public class Dog extends Animal {
+             public void sound() {
+            	 System.out.println("This is the sound of the dog : Bark");
+             }
+}
---
+++ CoreJava/src/oops/Polymorphism/dynamicdispatch/Features.java
@@ -0,0 +1,12 @@
+package oops.Polymorphism.dynamicdispatch;
+
+public class Features {
+             public static void main(String[] args) {
+				Animal a;
+				a =new Animal();
+				
+				a = new Dog();
+				a.sound();
+				
+			}
+}
