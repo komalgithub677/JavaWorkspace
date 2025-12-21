a12d03aad56ff38903933060f85fe846cc46b42e
Komal Narawade <knarawade4@gmail.com>
2025-12-21 15:33:51 +0530

    Day 42

---
+++ CoreJava/src/encapsulation/EmployeesInfo.java
@@ -0,0 +1,8 @@
+package encapsulation;
+
+public class EmployeesInfo {
+             public static void main(String[] args) {
+				EmployeesInfo e  = new EmployeesInfo();
+//				e.getemployeeId;
+			}
+}
