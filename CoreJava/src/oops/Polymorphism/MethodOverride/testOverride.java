5767e2fbb890de8584e05620f69873f5e55dba7b
Komal Narawade <knarawade4@gmail.com>
2025-12-21 17:53:14 +0530

    Day 43

---
+++ CoreJava/src/oops/Polymorphism/MethodOverride/testOverride.java
@@ -0,0 +1,9 @@
+package oops.Polymorphism.MethodOverride;
+
+public class testOverride {
+             public static void main(String[] args) {
+				Animals a;
+				a = new Dogs();
+				a.sound();
+			}
+}
