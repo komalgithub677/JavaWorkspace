299b4b0d0a91e826ffc50d0673911e086bdf32f6
Komal Narawade <knarawade4@gmail.com>
2025-12-25 15:58:39 +0530

    Day 47

---
+++ CoreJava/src/accessspecifier/Vehicles.java
@@ -0,0 +1,13 @@
+package accessspecifier;
+
+public class Vehicles {
+//	Create class Vehicle:
+//
+//		default method run()
+//
+//		Create class Car in same package and call run().
+	
+	void run() {
+		System.out.println("This is the run method of the run class");
+	}
+}
