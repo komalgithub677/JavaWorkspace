25f1c1b48929b18315d9bf4257d79d517e842ba6
Komal Narawade <knarawade4@gmail.com>
2025-12-25 15:26:41 +0530

    Day 47

---
+++ CoreJava/src/accessspecifier/CInfo.java
@@ -0,0 +1,10 @@
+package accessspecifier;
+
+public class CInfo {
+             public static void main(String[] args) {
+//				C c = new C();
+            	 //when trying to access the private constructor get an error
+            	 
+				
+			}
+}
