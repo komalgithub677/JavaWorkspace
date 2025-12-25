e81d068ec3b592a7a7ef3b335f9d0f4f6a0cd45a
Komal Narawade <knarawade4@gmail.com>
2025-12-25 15:18:38 +0530

    Day 47

---
+++ CoreJava/src/accessspecifier1/TestInfo.java
@@ -0,0 +1,10 @@
+package accessspecifier1;
+
+import accessspecifier.Test;
+
+public class TestInfo {
+          public static void main(String[] args) {
+			Test t = new Test();
+			t.display();
+		}
+}
