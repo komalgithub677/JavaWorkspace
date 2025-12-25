11e662365c89e3c56cd4aef68c68beaaec691c3d
Komal Narawade <knarawade4@gmail.com>
2025-12-25 14:58:07 +0530

    Day 47

---
+++ CoreJava/src/accessspecifier/Demos.java
@@ -0,0 +1,7 @@
+package accessspecifier;
+
+public class Demos {
+             public void show() {
+            	    System.out.println("This is the show method of the Demos class");
+             }
+}
