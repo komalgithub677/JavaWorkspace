e95341febdd71c3c4f9b162cc699b76133b9f0d5
Komal Narawade <knarawade4@gmail.com>
2025-12-26 15:35:45 +0530

    Day 48

---
+++ CoreJava/src/oops/practicequestions/Animal.java
@@ -0,0 +1,7 @@
+package oops.practicequestions;
+
+public class Animal {
+             public void sound() {
+            	 System.out.println("This is method of the Aniaml Class");
+             }
+}
---
+++ CoreJava/src/oops/practicequestions/Dog.java
@@ -0,0 +1,7 @@
+package oops.practicequestions;
+
+public class Dog extends Animal {
+             public void sound() {
+            	 System.out.println("Bark...");
+             }
+}
