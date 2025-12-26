0d1899c1518c88b74533bfee1dc1819276f6ed9a
Komal Narawade <knarawade4@gmail.com>
2025-12-26 17:59:09 +0530

    Day 48

---
+++ CoreJava/src/oops/practicequestions/DegreeCourse.java
@@ -0,0 +1,5 @@
+package oops.practicequestions;
+
+public class DegreeCourse {
+             
+}
---
+++ CoreJava/src/oops/statickeyword/Demo.java
@@ -0,0 +1,33 @@
+package oops.statickeyword;
+
+public class Demo {
+	
+	         static String name="rahul";
+	         int age;
+	         String city;
+	         
+	         //constructor
+		        Demo(){
+		        	System.out.println("Demo constructor");
+		        }
+	         
+	         static {
+	        	 System.out.println("This is static block");
+	         }
+	         
+	         {
+	        	 System.out.println("Non-static");
+	         }
+	        public static void display() {
+	        	 System.out.println("This is Static Method");
+	         }
+	        
+	       
+             public static void main(String[] args) {
+            	 Demo d = new Demo();
+				    Demo.display();
+				    
+				    
+				    
+			}
+}
