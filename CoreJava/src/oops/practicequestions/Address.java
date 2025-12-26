9440f2384f658b91a89a92af6f13d0a9b748ade4
Komal Narawade <knarawade4@gmail.com>
2025-12-26 16:46:49 +0530

    Day 48

---
+++ CoreJava/src/oops/practicequestions/Address.java
@@ -0,0 +1,18 @@
+package oops.practicequestions;
+
+public class Address {
+             String city;
+             String state;
+			public Address(String city, String state) {
+				super();
+				this.city = city;
+				this.state = state;
+			}
+			@Override
+			public String toString() {
+				return "Address [city=" + city + ", state=" + state + "]";
+			}
+             
+             
+             
+}
---
+++ CoreJava/src/oops/practicequestions/Vehicle.java
@@ -0,0 +1,9 @@
+package oops.practicequestions;
+
+public class Vehicle {
+             final double max_speed =120;
+             
+             final void start() {
+            	 System.out.println("This is the final method of the vehicle class");
+             }
+}
