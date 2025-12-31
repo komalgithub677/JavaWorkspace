70fdbf91fd13df1a37b5688bb135afea97c411c3
Komal Narawade <knarawade4@gmail.com>
2025-12-31 07:49:17 +0530

    Day 52

--- CoreJava/src/string/StringPracticeQuestions.java
+++ CoreJava/src/string/StringPracticeQuestions.java
@@ -15,10 +15,15 @@ public class StringPracticeQuestions {
 //                        		   Find length of string using length() method. 
                         	   System.out.println("Length of String : " + s.length());
 //                        		   Convert string to uppercase. 
-                        	   System.out.println("UUppercase : " + s.toUpperCase());
+                        	   System.out.println("Uppercase : " + s.toUpperCase());
 //                        		   Convert string to lowercase. 
+                        	   System.out.println("LowerCase : " + s.toLowerCase());
 //                        		   Check whether two strings are equal or not. 
+                        	   String s1 = "java";
+                        	   String s2 = "java";
+                        	   System.out.println("Strings equality : "+ s1.equals(s2));
 //                        		   Concatenate two strings without using + operator.
+                        	   
 //                        		   Check whether a string contains the word "Java". 
 //                        		   Find the first and last character of a string.
 						}
