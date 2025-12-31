87f7f2f1011db79346d93a796292a0949467b292
Komal Narawade <knarawade4@gmail.com>
2025-12-31 07:46:17 +0530

    Day 52

--- CoreJava/src/string/StringPracticeQuestions.java
+++ CoreJava/src/string/StringPracticeQuestions.java
@@ -11,8 +11,11 @@ public class StringPracticeQuestions {
                         	   for(int i=0; i<=s.length()-1; i++) {
                         		   System.out.println("Char : " + s.charAt(i));
                         	   }
+                        	   
 //                        		   Find length of string using length() method. 
+                        	   System.out.println("Length of String : " + s.length());
 //                        		   Convert string to uppercase. 
+                        	   System.out.println("UUppercase : " + s.toUpperCase());
 //                        		   Convert string to lowercase. 
 //                        		   Check whether two strings are equal or not. 
 //                        		   Concatenate two strings without using + operator.
