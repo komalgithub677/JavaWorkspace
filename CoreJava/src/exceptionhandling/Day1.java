f7c4cef099a9ce20d3c347e8fd4282be3fde7829
Komal Narawade <knarawade4@gmail.com>
2026-01-05 17:38:45 +0530

    Day 55

---
+++ CoreJava/src/exceptionhandling/Day1.java
@@ -0,0 +1,13 @@
+package exceptionhandling;
+
+public class Day1 {
+             public static void main(String[] args) {
+				System.out.println(4/2);
+				System.out.println(2/2);
+				System.out.println(6/2);
+				System.out.println(4/0);
+				System.out.println(12/2);
+				System.out.println(6/2);
+				System.out.println(8/2);
+			}
+}
