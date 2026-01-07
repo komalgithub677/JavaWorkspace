8139e3e23b267e880a1dcf7bd291cd06e7890b18
Komal Narawade <knarawade4@gmail.com>
2026-01-07 20:27:32 +0530

    Day 56

---
+++ CoreJava/src/multithreading/HelloThread.java
@@ -0,0 +1,13 @@
+package multithreading;
+
+public class HelloThread extends Thread {
+//	Write a program to print “Hello from Thread class” using Thread.
+	public void run() {
+		System.out.println("This is the helloThread class...");
+	}
+	
+	public static void main(String[] args) {
+		HelloThread h =new HelloThread();
+		h.start();
+	}
+}
