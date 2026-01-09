3c294197ba8de26377a5fa565e99cbaf8c522d19
Komal Narawade <knarawade4@gmail.com>
2026-01-09 23:13:16 +0530

    Day 57

---
+++ CoreJava/src/practice/ThreadExample.java
@@ -0,0 +1,24 @@
+package practice;
+
+public class ThreadExample extends Thread {
+             public void run() {
+            	 for(int i=1; i<=10; i++) {
+            		 System.out.println("I : "+ i)  ;
+            	 }
+            	 
+            	 try {
+					sleep(2000);
+				} catch (InterruptedException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+             }
+             public static void main(String[] args) {
+				ThreadExample t = new ThreadExample();
+				t.start();
+				
+				for(int i=1; i<=10; i++) {
+					System.out.println("2's table : " + (2*i));
+				}
+			}
+}
