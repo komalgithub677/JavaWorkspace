ad3c927d386d8a498ada67d7ec0f947227fa97ca
Komal Narawade <knarawade4@gmail.com>
2026-01-11 17:59:31 +0530

    Day 59

--- CoreJava/src/practice/Practice1/PracticeQuestions2.java
+++ CoreJava/src/practice/Practice1/PracticeQuestions2.java
@@ -18,12 +18,70 @@ public class PracticeQuestions2 {
 	}
 //
 //	Count the occurrence of a digit in a number.
+	public void DigitFrequency(int n , int targetDigit) {
+		int digitFreq = 0;
+		
+		for(int i=n ;i>0; i=i/10) {
+			int digit = i%10;
+			if(digit == targetDigit) {
+				digitFreq++;
+			}
+		}
+		System.out.println("DigitFrequency would be : " + digitFreq);
+		
+		
+		
+	}
 //
 //	Print all factors of a given number.
+	
+	public void factors(int n) {
+		   int factors =0;
+		   
+		   for(int i=1; i<=n; i++) {
+			   if(n%i==0) {
+				   System.out.println("Factors  " + i);
+			   }
+			   
+		   }
+	}
 //
 //	Check whether a number is Armstrong number.
+	public void armStrongNumber(int n) {
+		        int original = n;
+		        int digits = 0;
+		        int sum =0;
+		        
+		        for(int temp=n; temp>0; temp= temp/10) {
+		        	digits++;
+		        }
+		        
+		        //calculate the armstrong number
+		        for(int temp=n ;temp>0; temp = temp/10) {
+		        	int digit = temp%10;
+		        	sum +=Math.pow(digits, digit);
+		        }
+		        
+		        if(sum==original) {
+		        	System.out.println("Armstrong number  : " + original);
+		        }
+		        else {
+		        	System.out.println("Not a ArmStrong number : " + original);
+		        }
+	}
 //
 //	Find GCD (HCF) of two numbers using loop.
+	public void findGCD(int a ,int b) {
+		int gcd =1;
+		
+		for(int i=1; i<=a&& i<=b; i++) {
+			if(a%i==0 && b%i==0) {
+				gcd =i;
+			}
+		}
+		
+		System.out.println("GCD = " + gcd);
+	}
 //
 //	Find LCM of two numbers using loop.
 //
@@ -34,5 +92,19 @@ public class PracticeQuestions2 {
 				System.out.println("Enter number : ");
 				int n = sc.nextInt();
 				p.largestDigit(n);
+				
+				System.out.println("Enter the target digit : " );
+				p.DigitFrequency(n, 3);
+				
+				p.factors(n);
+				
+				p.armStrongNumber(n);
+				
+				System.out.println("Enter a : ");
+				int a = sc.nextInt();
+				System.out.println("Enter b : ");
+				int b = sc.nextInt();
+				
+				p.findGCD(a,b);
 			}
 }
---
+++ CoreJava/src/practice/Practice1/PracticeQuestions3.java
@@ -0,0 +1,86 @@
+package practice.Practice1;
+
+import java.util.Scanner;
+
+public class PracticeQuestions3 {
+//	Print the reverse of a string using loop (without inbuilt methods).
+	public void reverseString(String s) {
+		String reverse = "";
+		for(int i=s.length()-1; i>=0; i--) {
+			reverse = reverse + s.charAt(i);
+		}
+		System.out.println("Reverse String : " + reverse);
+	}
+//
+//	Count vowels and consonants in a string using loop.
+	
+	public void vowelConsonant() {
+		char ch ='a';
+		if(ch=='A' || ch=='I' || ch=='O' || ch=='U' || ch=='E' || ch=='a' || ch=='i' || ch=='o' || ch=='u' || ch=='e') {
+			System.out.println("Character is Vowel :" + ch);
+		}
+		else {
+			System.out.println("Character is consonant");
+		}
+	}
+//
+//	Find duplicate characters in a string using loop.
+	public void findDuplicateChars(String s) {
+
+	    char[] ch = s.toCharArray();
+
+	    System.out.println("Duplicate characters:");
+
+	    for (int i = 0; i < ch.length; i++) {
+
+	        if (ch[i] == ' ') continue; // skip spaces
+
+	        int count = 1;
+
+	        for (int j = i + 1; j < ch.length; j++) {
+	            if (ch[i] == ch[j]) {
+	                count++;
+	                ch[j] = '0'; // mark visited
+	            }
+	        }
+
+	        if (count > 1 && ch[i] != '0') {
+	            System.out.println(ch[i] + " -> " + count);
+	        }
+	    }
+	}
+
+//
+//	Check whether a string is Palindrome using loop.
+	public void palindromeString(String s) {
+		String original = s;
+		
+		String reverse = "" ;
+		for(int i=s.length()-1; i>0; i--) {
+			reverse = reverse + s.charAt(i);
+		}
+		if(original.equals(reverse)) {
+			System.out.println("String is palindrome");
+		}
+		else {
+			System.out.println("String is not palindrome");
+		}
+	}
+//
+//	Find the frequency of each character in a string.
+//
+//	Remove spaces from a string using loop.
+	
+	public static void main(String[] args) {
+		PracticeQuestions3 p = new PracticeQuestions3();
+		Scanner sc = new Scanner(System.in);
+		String s = "Komal";
+		p.reverseString(s);
+		
+		p.vowelConsonant();
+		
+		p.findDuplicateChars(s);
+		
+		p.palindromeString(s);
+	}
+}
