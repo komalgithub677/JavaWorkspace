772b7dc23ba59a9aa75fe09cd200eee4d24abb5c
Komal Narawade <knarawade4@gmail.com>
2026-01-14 12:08:12 +0530

    Day 62

---
+++ CoreJava/src/arrays/CopyArrayElements.java
@@ -0,0 +1,33 @@
+package arrays;
+
+import java.util.Scanner;
+
+public class CopyArrayElements {
+             public void copy(int arr[]) {
+            	 
+            	 int arr1[] = new int[arr.length];
+            	 for(int i=0; i<arr.length; i++) {
+            		 arr1[i] = arr[i];
+            		
+            	 }
+            	 for(int i=0; i<arr1.length; i++) {
+            		 System.out.println(arr1[i]);
+            	 }
+            	 
+            	 
+             }
+             
+             public static void main(String[] args) {
+				Scanner sc = new Scanner(System.in);
+				CopyArrayElements c = new CopyArrayElements();
+				
+				int arr[] = new int[5];
+				System.out.println("Enter the original Array : ");
+				for(int i=0; i<arr.length; i++) {
+				     arr[i] = sc.nextInt();
+				}
+				
+				System.out.println("Copied array : ");
+				c.copy(arr);
+			}
+}
