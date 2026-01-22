24c36057c38485ff6b53b246d923c9b16fe2aa0a
Komal Narawade <knarawade4@gmail.com>
2026-01-22 16:54:08 +0530

    Day 67

---
+++ CoreJava/src/collectionsFrameworks/practice/SizeOfList.java
@@ -0,0 +1,19 @@
+package collectionsFrameworks.practice;
+
+import java.util.ArrayList;
+
+public class SizeOfList {
+             public static void main(String[] args) {
+				ArrayList <Integer> list = new ArrayList<>();
+				
+				list.add(10);
+				list.add(12);
+				list.add(14);
+				list.add(15);
+				list.add(17);
+				
+				System.out.println(list);
+				
+				System.out.println("Size of list : " + list.size());
+			}
+}
