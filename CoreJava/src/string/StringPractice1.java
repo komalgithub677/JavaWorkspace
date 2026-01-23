8dd7849a80e5a8effb4d9a19b54857fd7baf3757
Komal Narawade <knarawade4@gmail.com>
2026-01-23 06:55:38 +0530

    Day 68

---
+++ CoreJava/src/string/StringPractice1.java
@@ -0,0 +1,18 @@
+package string;
+
+public class StringPractice1 {
+              public static void main(String[] args) {
+//            	  Write a program to create a String and print it.
+            	  String s = "Komal";
+            	  System.out.println("String Value : " + s);
+//
+//            	  Take a string input from the user and display it.
+            	  
+//
+//            	  Find the length of a string.
+//
+//            	  Print each character of a string using a loop.
+//
+//            	  Check whether a string is empty or not.
+			}
+}
