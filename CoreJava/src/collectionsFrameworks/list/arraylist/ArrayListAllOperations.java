99f460aa4ee59c91ae396234cc0eec3040dce0bb
Komal Narawade <knarawade4@gmail.com>
2026-01-27 08:30:35 +0530

    Day 69

--- CoreJava/src/collectionsFrameworks/list/arraylist/ArrayListAllOperations.java
+++ CoreJava/src/collectionsFrameworks/list/arraylist/ArrayListAllOperations.java
@@ -1,6 +1,7 @@
 package collectionsFrameworks.list.arraylist;
 
 import java.util.ArrayList;
+import java.util.Collections;
 
 public class ArrayListAllOperations {
              public static void main(String[] args) {
@@ -18,5 +19,35 @@ public class ArrayListAllOperations {
 				//add method
 				list.add(30);
 				System.out.println(list);
+				
+				//remove method
+				list.remove(3);
+				System.out.println(list);
+				
+				//size method
+				System.out.println(list.size());
+				
+				//index of method
+				System.out.println(list.indexOf(12));
+				
+				//is empty method
+				System.out.println(list.isEmpty());
+				
+				//last index of list
+				System.out.println(list.lastIndexOf(18));
+				
+				//get first element
+				System.out.println(list.getFirst());
+				
+				//get last element
+				System.out.println(list.getLast());
+				
+				//Sort the arrayList
+				Collections.sort(list);
+				System.out.println(list);
+				
+				//sort the list reverse order
+				Collections.sort(list ,Collections.reverseOrder());
+				System.out.println(list);	
 			}
 }
