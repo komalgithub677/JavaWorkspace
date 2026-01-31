522ed59288626518182fb4b2a4dc03015549761d
Komal Narawade <knarawade4@gmail.com>
2026-01-31 21:53:52 +0530

    Day 71

--- CoreJava/src/arrays/ArraysPracticeQ.java
+++ CoreJava/src/arrays/ArraysPracticeQ.java
@@ -17,12 +17,64 @@ public class ArraysPracticeQ {
 	}
 //
 //	Find the smallest element in an array.
+	
+	public void smallest(int arr[]) {
+		int min = Integer.MAX_VALUE;
+		
+		for(int i=0; i<arr.length; i++) {
+			if(arr[i]<min) {
+				min = arr[i];
+			}
+		}
+		
+		System.out.println("Minimum element : " + min);
+	}
 //
 //	Count how many even and odd numbers are in the array.
+	public void countEvenOdd(int arr[]) {
+		int countEven = 0;
+		int countOdd = 0;
+		
+		for(int i=0; i<arr.length; i++) {
+			if(arr[i]%2 ==0) {
+				countEven++;
+			}
+			else {
+				countOdd++;
+			}
+		}
+		
+		System.out.println("Count of even : " + countEven);
+		System.out.println("Count of odd : " + countOdd);
+	}
 //
 //	Calculate the sum of all elements in an array.
+	
+	public void sum(int arr[]) {
+		int sum = 0;
+		for(int i=0; i<arr.length; i++) {
+			sum +=arr[i];
+		}
+		
+		System.out.println("Sum of elements : " + sum);
+	}
 //
 //	Print the array in reverse order.
+	public void reverse(int arr[]) {
+		
+		int start = 0;
+		int end = arr.length-1;
+		
+		for(int i=0; i<arr.length; i++) {
+			while(start<=end) {
+				int temp = arr[start];
+				arr[start] = arr[end];
+				arr[end] = temp;
+				start ++;
+				end --;
+			}
+		}
+	}
 //
 //	Check whether a given element exists in the array (linear search).
 //
@@ -42,5 +94,17 @@ public class ArraysPracticeQ {
 			arr[i] = sc.nextInt();
 		}
 		a.largest(arr);
+		
+		a.smallest(arr);
+		
+		a.countEvenOdd(arr);
+		
+		a.sum(arr);
+		
+		a.reverse(arr);
+		
+		for(int i : arr) {
+			System.out.println(i + " ");
+		}
 	}
 }
