5df1b889204085b7a4bf424a6ad96dc414ef6915
Komal Narawade <knarawade4@gmail.com>
2026-02-11 23:29:48 +0530

    Day 76

--- CoreJava/src/collectionsFrameworks/list/arraylist/Practice.java
+++ CoreJava/src/collectionsFrameworks/list/arraylist/Practice.java
@@ -7,8 +7,46 @@ import java.util.Vector;
 public class Practice {
              public static void main(String[] args) {
 	         ArrayList <Integer> list = new ArrayList<>();
-	         Vector<Integer> list1 = new Vector<>();
-	         LinkedList<Integer> list2 = new LinkedList<>();
+	         list.add(10);
+	         list.add(11);
+	         list.add(12);
+	         list.add(13);
+	         list.add(14);
+	         list.add(15);
+	         list.add(16);
+	         list.add(17);
+	         list.add(18);
+	         
+	         System.out.println(list);
+	         
+	         Vector<Integer> vector = new Vector<>();
+	         vector.add(14);
+	         vector.add(15);
+	         vector.add(16);
+	         vector.add(17);
+	         vector.add(18);
+	         vector.add(19);
+	         vector.add(19);
+	         vector.add(13);
+	         vector.add(10);
+	         
+	         System.out.println(vector);
+	         
+	         LinkedList<Integer> link = new LinkedList<>();
+	         
+	         link.add(34);
+	         link.add(35);
+	         link.add(38);
+	         link.add(39);
+	         link.add(33);
+	         link.add(36);
+	         link.add(38);
+	         link.add(35);
+	         link.add(36);
+	         link.add(38);
+	         
+	         System.out.println(link);
+	         
 	         
 	         
 	         
