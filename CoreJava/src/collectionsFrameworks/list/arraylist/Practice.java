8c8f617aabf7a55c496bb102ddafd53158013392
Komal Narawade <knarawade4@gmail.com>
2026-02-11 23:00:59 +0530

    Day 76

---
+++ CoreJava/src/collectionsFrameworks/list/arraylist/Practice.java
@@ -0,0 +1,16 @@
+package collectionsFrameworks.list.arraylist;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.Vector;
+
+public class Practice {
+             public static void main(String[] args) {
+	         ArrayList <Integer> list = new ArrayList<>();
+	         Vector<Integer> list1 = new Vector<>();
+	         LinkedList<Integer> list2 = new LinkedList<>();
+	         
+	         
+	         
+}
+}
