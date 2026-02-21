1bfa9692be8623e1219e1091d51b8f39fdb961d7
Komal Narawade <knarawade4@gmail.com>
2026-02-21 19:10:36 +0530

    Day 82

--- CoreJava/src/QuestionsPractice.java
+++ CoreJava/src/QuestionsPractice.java
@@ -2,6 +2,13 @@ import java.util.Scanner;
 
 
 public class QuestionsPractice {
+	         public void fact(int n) {
+	        	 int fact =1;
+	        	 for(int i=1; i<=n; i++) {
+	        		 fact *=i;
+	        	 }
+	        	 System.out.println("Factorial : " + fact);
+	         }
              
              
              public static void main(String[] args) {
@@ -14,5 +21,28 @@ public class QuestionsPractice {
 				else {
 					System.out.println("Odd");
 				}
+				
+				//Check prime number
+				boolean isPrime = true;
+				
+				if(n<=1) {
+					isPrime =false;
+				}
+				
+				for(int i=2; i<=Math.sqrt(n); i++) {
+					if(n%i==0) {
+						isPrime =false;
+						break;
+					}
+				}
+				if(isPrime =true) {
+					System.out.println("Prime number");
+				}
+				else {
+					System.out.println("Not prime number");
+				}	
+				
+				//factorial
+				q.fact(n);
 }
 }
