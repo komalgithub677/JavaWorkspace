f114869001b2ccb59881a1ec6145c58b2e3f3ad2
Komal Narawade <knarawade4@gmail.com>
2026-03-09 00:24:02 +0530

    Day 90

--- CoreJava/src/NQTQuestionsPractice.java
+++ CoreJava/src/NQTQuestionsPractice.java
@@ -9,8 +9,34 @@ public class NQTQuestionsPractice {
             		 System.out.println("Odd");
             	 }
              }
+             
+             public void primeNumber() {
+            	 int num =12;
+            	 boolean isPrime = false;
+            	 
+            	 if(num==1) {
+            		 System.out.println("Not prime");
+            	 }
+            	 
+            	 for(int i=2; i<Math.sqrt(num); i++) {
+            		 if(num%i==0) {
+            			 isPrime = false;
+            			 break;
+            		 }
+            	 }
+            	 if(isPrime==true) {
+            		 System.out.println("Number is prime");
+            	 }
+            	 else {
+            		 System.out.println("Number is not prime");
+            	 }
+            	 
+             }
+             
+             
              public static void main(String[] args) {
             	 NQTQuestionsPractice  n = new NQTQuestionsPractice ();
             	 n.checkEvenOdd();
+            	 n.primeNumber();
 			}
 }
