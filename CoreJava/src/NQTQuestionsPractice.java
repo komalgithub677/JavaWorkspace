65744af2486f1f3cd291ca05a326dcda732c0430
Komal Narawade <knarawade4@gmail.com>
2026-03-09 00:18:46 +0530

    Day 90

---
+++ CoreJava/src/NQTQuestionsPractice.java
@@ -0,0 +1,16 @@
+
+public class NQTQuestionsPractice {
+             public void checkEvenOdd() {
+            	 int n =12;
+            	 if(n%2==0) {
+            		 System.out.println("Even");
+            	 }
+            	 else {
+            		 System.out.println("Odd");
+            	 }
+             }
+             public static void main(String[] args) {
+            	 NQTQuestionsPractice  n = new NQTQuestionsPractice ();
+            	 n.checkEvenOdd();
+			}
+}
