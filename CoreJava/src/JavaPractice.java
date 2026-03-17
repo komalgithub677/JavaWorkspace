6ad8739c800c83867c37615dbcaa8809d15b007c
Komal Narawade <knarawade4@gmail.com>
2026-03-17 18:12:47 +0530

    Day 95

--- CoreJava/src/JavaPractice.java
+++ CoreJava/src/JavaPractice.java
@@ -1,13 +1,20 @@
 
 public class JavaPractice {
+	//which are declared inside a class and the outside the methods
        int id;
        String name;
        String city;
        
+       //which are declared inside the class but 
+       //this are the class level if we have to access this variable or call this variable we can call 
+       //that only with the class name
        static String college = "Alard";
        
        
        void display() {
+    	   //local variables which are delclared inside the method 
+    	   //we cannot declared this variables without a value 
+    	   //we have to initialized the some value to the variables 
     	   int a =10;
     	   System.out.println(a);
        }
