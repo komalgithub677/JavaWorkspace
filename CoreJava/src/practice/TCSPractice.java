51eee37e01820291a4307124262ae60a5bfd921a
Komal Narawade <knarawade4@gmail.com>
2026-03-23 02:28:20 +0530

    Add findGrade class to determine student grades

---
+++ CoreJava/src/practice/TCSPractice.java
@@ -0,0 +1,29 @@
+public class findGrade{
+    public void grade(){
+        int marks =50;
+        
+        if(marks<=90){
+            System.out.println("Grade A");
+        }
+        else if(marks<=80){
+            System.out.println("Grade B");
+        }
+        else if(marks<=70){
+            System.out.println("Grade C");
+        }
+        else if(marks<=60){
+            System.out.println("Grade D");
+        }
+        else if(marks<50){
+            System.out.println("Grade E");
+        }
+        else{
+            System.out.println("Fail...");
+        }
+    }
+    
+    public static void main(String [] args){
+        findGrade f = new findGrade();
+        f.grade();
+    }
+}
